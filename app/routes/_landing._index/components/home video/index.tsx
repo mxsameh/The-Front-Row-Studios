1e42ff66fcfed0dfe7aa8d2b3ef30a1a764d47a1
Mohamed <mxsameh@gmail.com>
2023-11-23 19:52:29 +0200

    added to home

--- app/routes/_landing._index/components/home video/index.tsx
+++ app/routes/_landing._index/components/home video/index.tsx
@@ -1,8 +1,12 @@
 import homeVideo from '~/assets/videos/FR-Video.mp4';
+import heroImage from '~/assets/images/hero.webp';
 import styles from './styles.module.css';
 const HomeVideo = () => {
   return (
     <>
+      <div className={styles.heroImageContainer}>
+        <img src={heroImage} alt="FR-Hero" className={styles.heroImage} />
+      </div>
       <video autoPlay loop className={styles.video}>
         <source src={homeVideo} />
       </video>
